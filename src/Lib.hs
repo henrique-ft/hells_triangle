ef3b5086c23e5972ea68fcb84ce54027b2f8c9cb
henriquefernandez <hriqueft@gmail.com>
2018-01-03 16:07:21 -0200

    firt functions and dependencies

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
 module Lib
-    ( someFunc
+    ( 
+      startProgram
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type HellsTriangle = [[Int]]
+
+-- Programn Functions
+
+getUserInput :: IO HellsTriangle
+getUserInput = do
+    name <- getLine 
+    return (read name) :: IO HellsTriangle 
+
+
+findMaximumTotal :: HellsTriangle -> IO ()
+findMaximumTotal hellsTriangle = do
+    return ()
+
+startProgram :: IO ()
+startProgram = do
+    putStrLn "-------------- Welcome to HellsTriangle Machine --------------" 
+    putStrLn ""
+    putStrLn "Send a triangle in a list and i'll give you the maximum total. "
+    putStrLn "Example: [[1],[1,2],[1,2,3]]"  
+    putStrLn ""
+    hellsTriangle <- getUserInput
+    findMaximumTotal hellsTriangle
+    return ()
