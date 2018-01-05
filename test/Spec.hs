12693955def6d7c911ce995a033e4abc0fa7bf76
henriquefernandez <hriqueft@gmail.com>
2018-01-05 11:28:14 -0200

    mapWithIteration tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Lib
 main :: IO ()
 main = hspec $ do -- A partir desse primeiro do teremos uma ou mais descrições do teste
 	describe "returnValueByIndexOrReturnZero function" $ do -- A partir daqui teremos uma ou mais afirmações do teste
+
 		it "Return a correct value by list index" $ do 
 			(returnValueByIndexOrReturnZero 3 [1,2,3,4]) `shouldBe` 4
 
@@ -13,4 +14,11 @@ main = hspec $ do -- A partir desse primeiro do teremos uma ou mais descrições
 		it "Return zero if index is negative" $ do 
 			(returnValueByIndexOrReturnZero (-1) [1,2,3,4]) `shouldBe` 0
 
+	describe "mapWithIteration works" $ do
+		
+		it "receive a function and a list" $ do
+			(mapWithIteration (\k v -> v+1) [1,2,3,4]) `shouldBe` [2,3,4,5]
+
+		it "receive the index of iteration" $ do
+			(mapWithIteration (\k v -> v+k) [1,2,3,4]) `shouldBe` [1,3,5,7]
 	
